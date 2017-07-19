5df30045ffe504a77f72a2bc0d501c27875ffc70
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-07-19 12:34:30 +0530

    Basic structure tested Working fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConentComponent } from './components/content.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/content.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'content',
+  templateUrl: '../views/content.component.html',
+  styleUrls: ['../views/content.component.css']
+})
+export class ConentComponent { }
