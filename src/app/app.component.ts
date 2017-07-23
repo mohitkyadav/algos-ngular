63a499d169d754e6203da84a247be4a9c046f44e
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-07-23 10:58:11 +0530

    blueprint is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,7 @@ import { trigger, transition, state, style, animate } from "@angular/animations"
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('tr1', [
-      state('smaller', style({
-        transform: 'scale(0)'
-      })),
-      state('larger', style({
-        transform: 'scale(3)'
-      })),
-      transition('smaller <-> larger', animate('300ms ease-in'))
-    ])
-  ]
 })
 export class AppComponent {
-  title = 'app';
-  state: string = 'smaller';
-  animate() {
-    this.state = this.state === 'larger' ? 'smaller' : 'larger';
-  }
+  title = 'algos';
 }
